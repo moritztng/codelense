cc1d0adaf75320fb52648d1e394725d4973c6b1a
Moritz Thüning <moritz.thuening@gmail.com>
2023-06-26 02:45:21 +0200

    Add cumulative star chart

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,78 +11,96 @@ import { LocalizationProvider } from '@mui/x-date-pickers'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { DatePicker } from '@mui/x-date-pickers/DatePicker'
 import { useQuery, gql } from '@apollo/client'
+import { useState } from 'react'
+import Button from 'react-bootstrap/Button'
+
 import './App.css'
+import * as dayjs from 'dayjs'
 
 const GET_TIME_POINTS = gql`
-  query timePoints {
-    timePoints {
+  query timePoints($fromDate: Int!, $toDate: Int!) {
+    timePoints(fromDate: $fromDate, toDate: $toDate) {
       Time
-        Values {
-          Name
-          Value
+      Values {
+        Name
+        Value
+      }
     }
-  }}`;
+  }
+`
 
-function App() {
-  const { loading, error, data } = useQuery(GET_TIME_POINTS)
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>Error : {error.message}</p>
-  const data2 = [
-    {
-      name: 'Page A',
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: 'Page B',
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: 'Page C',
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: 'Page D',
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: 'Page E',
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: 'Page F',
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: 'Page G',
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-  ]
+interface FilterProps {
+  defaultFromDate: dayjs.Dayjs
+  defaultToDate: dayjs.Dayjs
+  onFilterChange: (filter: {
+    fromDate: dayjs.Dayjs
+    toDate: dayjs.Dayjs
+  }) => void
+}
 
+interface ChartProps {
+  fromDate: dayjs.Dayjs
+  toDate: dayjs.Dayjs
+}
+
+function Filter({
+  defaultFromDate,
+  defaultToDate,
+  onFilterChange,
+}: FilterProps) {
+  const [fromDate, setFromDate] = useState(defaultFromDate)
+  const [toDate, setToDate] = useState(defaultToDate)
   return (
     <>
-      <h1>{data.timePoints[0].Time}</h1>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DatePicker />
-        <DatePicker />
+        <DatePicker
+          value={fromDate}
+          onChange={(newValue) => newValue && setFromDate(newValue)}
+        />
+        <DatePicker
+          value={toDate}
+          onChange={(newValue) => newValue && setToDate(newValue)}
+        />
       </LocalizationProvider>
+      <Button
+        variant="primary"
+        onClick={() => onFilterChange({ fromDate: fromDate, toDate: toDate })}
+      >
+        Start
+      </Button>
+    </>
+  )
+}
+
+function Chart({ fromDate, toDate }: ChartProps) {
+  const { loading, error, data } = useQuery(GET_TIME_POINTS, {
+    variables: { fromDate: fromDate.unix(), toDate: toDate.unix() },
+  })
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error : {error.message}</p>
+  const chartData = data.timePoints.map((element: any) =>
+    Object.assign(
+      {
+        time: element.Time,
+      },
+      ...element.Values.map((value: any) => ({ [value.Name]: value.Value }))
+    )
+  )
+  const lastTimePoint = chartData.reduce((lastTimePoint: any, timePoint: any) => ({...lastTimePoint, ...timePoint}), {})
+  const firstTimePoint = {...lastTimePoint}
+  Object.keys(firstTimePoint).forEach(key => {
+    firstTimePoint[key] = 0
+  });
+  firstTimePoint["time"] = fromDate.unix()
+  chartData.unshift(firstTimePoint)
+  chartData.push(lastTimePoint)
+  const colors = ["red", "darkblue", "blue", "green", "purple", "brown", "orange", "pink", "black", "gray"]
+  return (
+    <>
       <LineChart
         width={500}
         height={300}
-        data={data2}
+        data={chartData}
         margin={{
           top: 5,
           right: 30,
@@ -91,18 +109,30 @@ function App() {
         }}
       >
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
+        <XAxis dataKey="time" type="number" domain={['dataMin', 'dataMax']} tickFormatter={unixTime => dayjs.unix(unixTime).format('DD/MM/YY')}/>
         <YAxis />
-        <Tooltip />
         <Legend />
-        <Line
-          type="monotone"
-          dataKey="pv"
-          stroke="#8884d8"
-          activeDot={{ r: 8 }}
-        />
-        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        {Object.keys(firstTimePoint).filter(key => key != "time").map((key: any, index: any) => {
+          return <Line type="basis" dataKey={key} stroke={colors[index]} connectNulls />
+        })}
       </LineChart>
+    </>
+  )
+}
+
+function App() {
+  const [filter, setFilter] = useState({
+    fromDate: dayjs().subtract(7, 'day'),
+    toDate: dayjs(),
+  })
+  return (
+    <>
+      <Filter
+        defaultFromDate={filter.fromDate}
+        defaultToDate={filter.toDate}
+        onFilterChange={setFilter}
+      />
+      <Chart fromDate={filter.fromDate} toDate={filter.toDate} />
     </>
   )
 }
